f699c1fe1e7163b536f68f0b49cd62a249ea8870
yadu007 <yadugkrishnan@gmail.com>
2017-07-28 14:11:30 +0530

    final

--- ls.c
+++ ls.c
@@ -1,33 +1,55 @@
-    #include<stdio.h>
+ #include<stdio.h>
 #include<dirent.h>
 #include<string.h>
 #include<stdlib.h>
-    int main(int argc,char **argv)
-    {
-    struct dirent **name;
-    int n;
+void recursive(const char *name, int indent);
+
+int main(int argc,char **argv)
+ {
+
+   struct dirent **name;
+   int n;
+//printf("%s",argv[1]);
    if (argc == 1)
-   {
-   n=scandir(".",&name,NULL,alphasort);
+    {
+     n=scandir(".",&name,NULL,alphasort);
+      while (n--)
+       {
+         printf("%s\n",name[n]->d_name);
+         free(name[n]);
+       }
+      free(name);
    }
-   else
-   {
-   n = scandir(argv[1], &name, NULL, alphasort);
-   }
-   if(n < 0)
-   {
- perror("scandir");
-   exit(EXIT_FAILURE);
-   }
-   else
-   {
-   while (n--)
-   {
-   printf("%s\n",name[n]->d_name);
-   free(name[n]);
-   }
-   free(name);
-   }
+  else if(argc==2 || argv[1]=="-R"){
+  //printf("sd\n");
+   recursive(".",0);
+  // return 0;
+
+    }
    exit(EXIT_SUCCESS);
    }
+
+void recursive(const char *name, int indent)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char path[1024];
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            sprintf(path,"%s",entry->d_name);
+            printf("%*s[[%s]]\n", indent, "", entry->d_name);
+            recursive(path, indent+3);
+        } else {
+            printf("%*s-> %s\n", indent, "", entry->d_name);
+        }
+    }
+    closedir(dir);
+}
+
 
